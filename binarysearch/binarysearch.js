ee3f3f7bf44f4ac05664bdc84c3fdaca1e61a421
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-25 18:09:25 +0200

    progression

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,15 +1,21 @@
 export function binarySearch(searchFor, values, printOutput = false) {
+  if (printOutput) {
+    console.log("Starting binary seach! :D");
+    console.log("---------------------------");
+  }
+
+  let iterations;
+  let found = false;
   let upperIndex = values.length - 1;
   let lowerIndex = 0;
   let middleIndex;
-  let found = false;
 
-  let iterations;
   for (iterations = 1; iterations < 200; iterations++) {
     middleIndex = Math.floor((upperIndex + lowerIndex) / 2);
     let middleValue = values[middleIndex];
 
     if (printOutput) {
+      console.log("iteration no.: " + iterations);
       console.log("upperIndex: " + upperIndex);
       console.log("lowerIndex: " + lowerIndex);
       console.log("middleIndex: " + middleIndex);
@@ -33,5 +39,11 @@ export function binarySearch(searchFor, values, printOutput = false) {
     }
   }
 
-  return { found, index: middleIndex, iterations };
+  const returnObject = { found, index: middleIndex, iterations };
+
+  if (printOutput) {
+    console.log(returnObject);
+  }
+
+  return returnObject;
 }
--- binarysearch/main.js
+++ binarysearch/main.js
@@ -3,6 +3,6 @@ import { linearSearch } from "./linearsearch.js";
 
 const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
 
-console.log(binarySearch(34, values));
+binarySearch(34, values, true);
 
 // console.log(linearSearch(33, values));
