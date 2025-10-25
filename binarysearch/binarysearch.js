57beec689e8cf986c7307355dbc40e589f219654
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-25 18:26:33 +0200

    created isArraySorted.js

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,6 +1,6 @@
 export function binarySearch(searchFor, values, printOutput = false) {
   if (printOutput) {
-    console.log("Starting binary seach! :D");
+    console.log("Starting binary search! :D");
     console.log("---------------------------");
   }
 
@@ -46,4 +46,15 @@ export function binarySearch(searchFor, values, printOutput = false) {
   }
 
   return returnObject;
+}
+
+// Function that checks if an array is sorted
+export function isArraySorted(values) {
+  for (let i = 0; i < values.length - 1; i++) {
+    let current = values[i];
+    let next = values[i + 1];
+    if (current > next) return false;
+  }
+
+  return true;
 }
--- binarysearch/main.js
+++ binarysearch/main.js
@@ -1,8 +1,10 @@
-import { binarySearch } from "./binarysearch.js";
+import { binarySearch, isArraySorted } from "./binarysearch.js";
 import { linearSearch } from "./linearsearch.js";
 
 const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
 
-binarySearch(34, values, true);
+// binarySearch(34, values, true);
 
 // console.log(linearSearch(33, values));
+
+console.log(isArraySorted([21, 22, 23, 22, 27, 28, 29, 31, 32, 34, 35]));
