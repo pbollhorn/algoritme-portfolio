92a8029d398d3b00e45fdcd5e17ba8c8a4a5cf53
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-25 16:22:38 +0200

    now have test.js

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,4 +1,4 @@
-function binarySearch(searchFor, values) {
+export function binarySearch(searchFor, values) {
   console.log("Vi starter binary search! :D");
 
   let upperIndex = values.length - 1;
@@ -10,27 +10,28 @@ function binarySearch(searchFor, values) {
   console.log("upperIndex: " + upperIndex);
   console.log("lowerIndex: " + lowerIndex);
 
-  while (found == false) {
-    
-    middleIndex = Math.floor((upperIndex - lowerIndex) / 2);
-    let middleValue = values[middleIndex];
-    console.log("middleValue: " + middleValue);
+  // while (found == false) {
+  //   middleIndex = Math.floor((upperIndex - lowerIndex) / 2);
+  //   let middleValue = values[middleIndex];
+  //   console.log("middleValue: " + middleValue);
 
-    if (middleValue == searchFor) {
-      found = true;
-    }
+  //   if (middleValue == searchFor) {
+  //     found = true;
+  //   }
 
-    if (middleValue > searchFor) {
-      upperIndex = upperIndex;
-      lowerIndex = middleIndex;
-    }
+  //   if (middleValue > searchFor) {
+  //     upperIndex = upperIndex;
+  //     lowerIndex = middleIndex;
+  //   }
+  // }
 
-
-  }
-
-  return middleIndex;
+  // return middleIndex;
+  found = false;
+  index = 5;
+  iterations = 22;
+  return { found, index, iterations };
 }
 
-const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
+// const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
 
-console.log("Found index: " + binarySearch(28, values));
+// console.log("Found index: " + binarySearch(28, values));
