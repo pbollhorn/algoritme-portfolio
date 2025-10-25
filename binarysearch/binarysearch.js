c41a539426fdd0df84682b121837c8df72ecffbf
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-25 17:59:24 +0200

    added optional parameter "printOutput"

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,22 +1,21 @@
-export function binarySearch(searchFor, values) {
-  console.log("Vi starter binary search! :D");
-
+export function binarySearch(searchFor, values, printOutput = false) {
   let upperIndex = values.length - 1;
   let lowerIndex = 0;
   let middleIndex;
   let found = false;
 
   let iterations;
-  for (iterations = 1; ; iterations++) {
+  for (iterations = 1; iterations < 200; iterations++) {
     middleIndex = Math.floor((upperIndex + lowerIndex) / 2);
-
-    console.log("upperIndex: " + upperIndex);
-    console.log("lowerIndex: " + lowerIndex);
-    console.log("middleIndex: " + middleIndex);
-
     let middleValue = values[middleIndex];
-    console.log("middleValue: " + middleValue);
-    console.log("---------------------------")
+
+    if (printOutput) {
+      console.log("upperIndex: " + upperIndex);
+      console.log("lowerIndex: " + lowerIndex);
+      console.log("middleIndex: " + middleIndex);
+      console.log("middleValue: " + middleValue);
+      console.log("---------------------------");
+    }
 
     if (searchFor == middleValue) {
       found = true;
