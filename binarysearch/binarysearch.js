1ac6bf6f54a2f832d9d4c461dfdb9bb79eff8234
Peter Bollhorn <pbollhorn@gmail.com>
2025-10-26 11:56:46 +0100

    progression

--- binarysearch/binarysearch.js
+++ binarysearch/binarysearch.js
@@ -1,23 +1,29 @@
 export function binarySearch(searchFor, values, print = false) {
   if (print) {
-    console.log("Starting binary search! :D");
+    console.log(`Starting binary search for value=${searchFor}`);
     console.log("---------------------------");
   }
 
-  let iterations;
   let found = false;
-  let upperIndex = values.length - 1;
+  let iterations;
   let lowerIndex = 0;
+  let upperIndex = values.length - 1;
   let middleIndex;
 
   for (iterations = 1; ; iterations++) {
-    middleIndex = Math.floor((upperIndex + lowerIndex) / 2);
+    // Calculate middleIndex
+    // Alternates between using floor and ceil to avoid endless loop
+    middleIndex = (upperIndex + lowerIndex) / 2;
+    if (iterations % 2) middleIndex = Math.floor(middleIndex);
+    else middleIndex = Math.ceil(middleIndex);
+
+    // Look up middle value
     let middleValue = values[middleIndex];
 
     if (print) {
-      console.log("Iteration no.: " + iterations);
-      console.log(`Upper index: ${upperIndex} value: ${values[upperIndex]}`);
+      console.log(`Iteration no.: ${iterations}`);
       console.log(`Lower index: ${lowerIndex} value: ${values[lowerIndex]}`);
+      console.log(`Upper index: ${upperIndex} value: ${values[upperIndex]}`);
       console.log(`Middle index: ${middleIndex} value: ${middleValue}`);
       console.log("---------------------------");
     }
@@ -28,6 +34,7 @@ export function binarySearch(searchFor, values, print = false) {
     }
 
     if (lowerIndex == upperIndex) {
+      middleIndex = -1;
       break;
     }
 
@@ -45,7 +52,7 @@ export function binarySearch(searchFor, values, print = false) {
   const returnObject = { found, index: middleIndex, iterations };
 
   if (print) {
-    console.log("Finished the search with this result:");
+    console.log(`Finished search for value=${searchFor} with this result:`);
     console.log(returnObject);
   }
 
--- binarysearch/main.js
+++ binarysearch/main.js
@@ -1,5 +1,16 @@
 import { binarySearch } from "./binarysearch.js";
 
-const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
+// const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
 
-binarySearch(27, values, true);
+// binarySearch(36, values, true);
+
+// create an array of all numbers from 1 to 100
+let data = [];
+for (let i = 1; i <= 100; i++) {
+  data[i - 1] = i;
+}
+
+console.log(data);
+
+
+binarySearch(101, data, true);
