5e70d40fe82ab2f72f14cfe6b312c1ccd633cc11
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-15 10:12:58 +0100

    progression

---
+++ shuntingyard/shunting.js
@@ -0,0 +1,65 @@
+import Queue from "https://pbollhorn.github.io/datastruktur-portfolio/queue/queue.js";
+import Stack from "https://pbollhorn.github.io/datastruktur-portfolio/stack/stack.js";
+
+// Pseudo code for shunting yard algorithm:
+// https://brilliant.org/wiki/shunting-yard-algorithm/
+
+const inputQueue = new Queue();
+const outputQueue = new Queue();
+const operatorStack = new Stack();
+
+const precedence = {
+  //   "^": 5,
+  "*": 4,
+  "/": 3,
+  "+": 2,
+  "-": 1,
+};
+
+//parseExpression læser en expression og putter den i inputQueue
+function parseExpression(expression) {
+  const splitted = expression.split(" ");
+  for (const element of splitted) {
+    if (isNaN(element)) {
+      // element er en operation
+      inputQueue.enqueue(element);
+    } else {
+      // element er et tal
+      inputQueue.enqueue(Number(element));
+    }
+  }
+}
+
+function shunting(inputString) {
+  parseExpression(inputString);
+  inputQueue.printQueue();
+
+  while (inputQueue.size() > 0) {
+    const token = inputQueue.dequeue();
+    console.log(token);
+    // If token is a number add it to outputQueue
+    if (typeof token === "number") outputQueue.enqueue(token);
+    // else token is an operator
+    while (precedence[operatorStack.peek()] > precedence[token]) {
+      const operator = operatorStack.pop();
+      outputQueue.enqueue(operator);
+    }
+    operatorStack.push(token);
+
+    const o1 = token;
+    const o2 = operatorStack.peek();
+
+    if (precedence[o2] >= precedence[o1]) {
+    }
+  }
+
+  while (operatorStack.size() > 0) {
+    const operator = operatorStack.pop();
+    outputQueue.enqueue(operator);
+  }
+  console.log("hello from end");
+  outputQueue.printQueue();
+}
+
+const inputString = "2 + 3";
+shunting(inputString);
