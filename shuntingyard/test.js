d532d86c0254c07091738986089e7fe345e28df9
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-16 15:07:41 +0100

    renamed shunting.js to shuntingyard.js

--- shuntingyard/test.js
+++ shuntingyard/test.js
@@ -1,18 +1,18 @@
 import assert from "assert";
-import shunting from "./shunting.js";
+import shuntingyard from "./shuntingyard.js";
 
 describe("Shunting yard algorithm", function () {
   describe("testing the algorithm", function () {
     it("Test case 0", function () {
-      const result = shunting("2 + 3 * 4");
+      const result = shuntingyard("2 + 3 * 4");
       assert.equal(result, "2 3 4 * +");
     });
     it("Test case 1", function () {
-      const result = shunting("85 - 31");
+      const result = shuntingyard("85 - 31");
       assert.equal(result, "85 31 -");
     });
     it("Test case 2", function () {
-      const result = shunting("3 + 4 * ( 5 - 2 )");
+      const result = shuntingyard("3 + 4 * ( 5 - 2 )");
       assert.equal(result, "3 4 5 2 - * +");
     });
   });
