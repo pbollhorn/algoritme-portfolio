0a02acd609052f5feca6317a692913a13c63414b
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-16 14:14:26 +0100

    progression

--- binarysearch/binarysearch_recursive.js
+++ binarysearch/binarysearch_recursive.js
@@ -4,14 +4,14 @@ export function binarySearchRecursive(
   searchFor,
   values,
   print = false,
-  startIndex = 0,
-  endIndex = values.length - 1,
+  lowerIndex = 0,
+  upperIndex = values.length - 1,
   iterations = 0
 ) {
   // Only log if print is true
   const log = print ? console.log : () => {};
 
-  if (startIndex > endIndex) {
+  if (lowerIndex > upperIndex) {
     // returnObject in case of failure
     const returnObject = { found: false, index: -1, iterations };
     log(`Finished search for value=${searchFor} with this result:`);
@@ -19,13 +19,17 @@ export function binarySearchRecursive(
     return returnObject;
   }
 
-  const middleIndex = Math.floor((startIndex + endIndex) / 2);
-  const middleValue = values[middleIndex];
   iterations++;
+  const middleIndex = Math.floor((lowerIndex + upperIndex) / 2);
+  const middleValue = values[middleIndex];
+
+  log(`Iteration no.: ${iterations}`);
+  log(`Lower index: ${lowerIndex} value: ${values[lowerIndex]}`);
+  log(`Upper index: ${upperIndex} value: ${values[upperIndex]}`);
+  log(`Middle index: ${middleIndex} value: ${middleValue}`);
+  log("---------------------------");
 
   if (middleValue === searchFor) {
-    // console.log("Success, middleIndex: " + middleIndex);
-    // return middleIndex;
     // returnObject in case of success
     const returnObject = { found: true, index: middleIndex, iterations };
     log(`Finished search for value=${searchFor} with this result:`);
@@ -34,21 +38,24 @@ export function binarySearchRecursive(
   }
 
   if (middleValue > searchFor) {
-    startIndex = startIndex;
-    endIndex = middleIndex - 1;
+    return binarySearchRecursive(
+      searchFor,
+      values,
+      print,
+      lowerIndex,
+      middleIndex - 1,
+      iterations
+    );
   }
 
   if (middleValue < searchFor) {
-    startIndex = middleIndex + 1;
-    endIndex = endIndex;
+    return binarySearchRecursive(
+      searchFor,
+      values,
+      print,
+      middleIndex + 1,
+      upperIndex,
+      iterations
+    );
   }
-
-  return binarySearchRecursive(
-    searchFor,
-    values,
-    print,
-    startIndex,
-    endIndex,
-    iterations
-  );
 }
