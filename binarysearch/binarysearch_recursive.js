015e8293e9898bb8c637c69ff4bb02231dd897b8
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-16 13:44:57 +0100

    function now takes optional print parameter

--- binarysearch/binarysearch_recursive.js
+++ binarysearch/binarysearch_recursive.js
@@ -2,24 +2,18 @@
 export function binarySearchRecursive(
   searchFor,
   values,
-  startIndex,
-  endIndex,
-  iterations
+  print = false,
+  startIndex = 0,
+  endIndex = values.length - 1,
+  iterations=0
 ) {
-  if (startIndex === undefined) {
-    startIndex = 0;
-  }
-  if (endIndex === undefined) {
-    startIndex = values.length - 1;
-  }
-  if (iterations === undefined) {
-    iterations = 0;
-  }
 
   const middleIndex = Math.floor((startIndex + endIndex) / 2);
   const middleValue = values[middleIndex];
   console.log(middleIndex);
   console.log(middleValue);
+
+  iterations++;
 
   if (startIndex > endIndex) {
     console.log("Failure");
@@ -27,8 +21,15 @@ export function binarySearchRecursive(
   }
 
   if (middleValue === searchFor) {
-    console.log("Success, middleIndex: " + middleIndex);
-    return middleIndex;
+    // console.log("Success, middleIndex: " + middleIndex);
+    // return middleIndex;
+    // returnObject in case of success
+    const returnObject = { found: true, index: middleIndex, iterations };
+    if (print) {
+      console.log(`Finished search for value=${searchFor} with this result:`);
+      console.log(returnObject);
+    }
+    return returnObject;
   }
 
   if (middleValue > searchFor) {
@@ -43,11 +44,11 @@ export function binarySearchRecursive(
     endIndex = endIndex;
   }
 
-  return binarySearchRecursive(searchFor, values, startIndex, endIndex, 666);
+  return binarySearchRecursive(searchFor, values, print, startIndex, endIndex, 666);
 }
 
 // Testing with an array of 11 numbers in sorted order
 const values = [21, 22, 23, 25, 27, 28, 29, 31, 32, 34, 35];
 
-const result = binarySearchRecursive(27, values, 0, 10, 666);
-console.log("THE RESULT IS: " + result);
+const result = binarySearchRecursive(27, values);
+console.log("THE RESULT IS: " + result.index);
