399e8cde4be629d39256dbab2323b69b974ac068
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-18 13:49:02 +0100

    progression

--- shuffle/fisheryates.js
+++ shuffle/fisheryates.js
@@ -2,4 +2,16 @@ export function fisheryates(array) {
   console.log(array);
 }
 
+function swap(a, b) {
+  const c = a;
+  a = b;
+  b = c;
+}
+
 fisheryates([1, 2, 3, 9999, 3333]);
+
+const a = 5;
+const b = 7;
+swap(a, b);
+console.log(a);
+console.log(b);
