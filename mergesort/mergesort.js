5f39e1c6e651f84bbd244d020330be02e255c8f6
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-22 12:46:04 +0100

    seems to work now with returnObject

--- mergesort/mergesort.js
+++ mergesort/mergesort.js
@@ -4,16 +4,26 @@ export function mergeSort(array, print = false) {
   const log = print ? console.log : () => {}; // Only log if print is true
 
   if (array.length <= 1) {
-    return array;
+    const returnObject = {
+      array: array,
+      iterations: 0,
+      sorted: true,
+    };
+    return returnObject;
   }
 
-  // Split array into part A and part B
   const middleIndex = Math.floor(array.length / 2);
   let arrayA = array.slice(0, middleIndex);
   let arrayB = array.slice(middleIndex);
 
-  arrayA = mergeSort(arrayA, print);
-  arrayB = mergeSort(arrayB, print);
+  arrayA = mergeSort(arrayA, print).array;
+  arrayB = mergeSort(arrayB, print).array;
+  const arrayC = merge(arrayA, arrayB);
 
-  return merge(arrayA, arrayB);
+  const returnObject = {
+    array: arrayC,
+    iterations: 0,
+    sorted: true,
+  };
+  return returnObject;
 }
