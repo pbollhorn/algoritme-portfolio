026e707293c37df0e76f647cba45bd38269b2048
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-22 12:34:11 +0100

    merge sort seems to work now

--- mergesort/main.js
+++ mergesort/main.js
@@ -1,12 +1,13 @@
 import { mergeSort } from "./mergesort.js";
 import { merge } from "./merge.js";
 
+// const listA = [1, 3, 7, 8];
+// const listB = [0, 2, 4, 5, 6, 9];
+// const listC = merge(listA, listB);
+// console.log(listC);
+
 // const list = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
-// const result = mergeSort(list, true);
-// console.log(result);
 
-const listA = [1, 3, 7, 8];
-const listB = [0, 2, 4, 5, 6, 9];
-const listC = merge(listA, listB);
-
-console.log(listC);
+const list = [5, 8, 2, 1, 0, -1, 4, 3, 9, 7, 6, -17];
+const result = mergeSort(list, true);
+console.log(result);
--- mergesort/mergesort.js
+++ mergesort/mergesort.js
@@ -1,8 +1,19 @@
+import { merge } from "./merge.js";
+
 export function mergeSort(array, print = false) {
   const log = print ? console.log : () => {}; // Only log if print is true
+
+  if (array.length <= 1) {
+    return array;
+  }
+
+  // Split array into part A and part B
+  const middleIndex = Math.floor(array.length / 2);
+  let arrayA = array.slice(0, middleIndex);
+  let arrayB = array.slice(middleIndex);
+
+  arrayA = mergeSort(arrayA, print);
+  arrayB = mergeSort(arrayB, print);
+
+  return merge(arrayA, arrayB);
 }
-
-
-
-
-
