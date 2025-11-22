70cde75e701788924b591d48794547713ca24f91
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-22 14:23:23 +0100

    improved comments

--- quicksort/quicksort.js
+++ quicksort/quicksort.js
@@ -1,30 +1,32 @@
 export function quickSort(array, print = false) {
   const log = print ? console.log : () => {}; // Only log if print is true
 
+  // Base case
   if (array.length <= 1) {
     return array;
   }
 
+  // Sets middle element to be pivot
   const middleIndex = Math.floor(array.length / 2);
   const pivot = array[middleIndex];
 
-  const smaller = [];
-  const larger = [];
-  const equal = [];
-
-  // split array into 3 subarrays:
+  // Split array into 3 subarrays:
   // - smaller than pivot
   // - larger than pivot
   // - equal to pivot
+  const smaller = [];
+  const larger = [];
+  const equal = [];
   for (const element of array) {
     if (element < pivot) smaller.push(element);
     else if (element > pivot) larger.push(element);
     else equal.push(element);
   }
 
+  // Sort subarrays using recursion
   const smallerSorted = quickSort(smaller, print);
   const largerSorted = quickSort(larger, print);
 
-  // return combined array
+  // Return combined array
   return [...smallerSorted, ...equal, ...largerSorted];
 }
