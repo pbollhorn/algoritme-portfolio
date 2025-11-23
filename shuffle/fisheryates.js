ea693c6e5ba05974da3322400a063d40e6502ec7
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-23 15:09:53 +0100

    progression

--- shuffle/fisheryates.js
+++ shuffle/fisheryates.js
@@ -1,30 +1,7 @@
-export function fisheryates(array) {
-  console.log(array);
-  for (let i = 1; i < array.length; i++) {
-    const j = random(i);
-    // swap(array[i], array[j]);
+export function fisherYates(array) {
+  for (let i = array.length - 1; i >= 1; i--) {
+    const j = Math.round(Math.random() * i);
+    // swap array[i] and array[j]
     [array[i], array[j]] = [array[j], array[i]];
   }
-  console.log(array);
 }
-
-function random(max) {
-  return Math.floor(Math.random() * max);
-}
-
-// TODO: Why does this not work
-function swap(a, b) {
-  const c = a;
-  a = b;
-  b = c;
-}
-
-fisheryates([1, 2, 3, 9999, 3333]);
-
-const a = 5;
-const b = 7;
-swap(a, b);
-console.log(a);
-console.log(b);
-
-console.log("Random: " + Math.random() * 10);
---
+++ shuffle/main.js
@@ -0,0 +1,5 @@
+import { fisherYates } from "./fisheryates.js";
+
+const list = [0, 1, 2, 3, 4, 5, 6];
+fisherYates(list);
+console.log(list);
