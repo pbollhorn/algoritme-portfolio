4c777897550265aa807689a17756366d55f9a723
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-23 15:15:33 +0100

    finished fisher yates

--- shuffle/fisheryates.js
+++ shuffle/fisheryates.js
@@ -1,5 +1,6 @@
 export function fisherYates(array) {
   for (let i = array.length - 1; i >= 1; i--) {
+    // set j to be a random integer such that 0 ≤ j ≤ i
     const j = Math.round(Math.random() * i);
     // swap array[i] and array[j]
     [array[i], array[j]] = [array[j], array[i]];
