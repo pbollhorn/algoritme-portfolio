5ed2e10feb9bf37b997896ee3d603f1bd998c58a
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-23 15:24:01 +0100

    improved test_isarraysorted.js

--- misc/test_isarraysorted.js
+++ misc/test_isarraysorted.js
@@ -1,55 +1,42 @@
 import assert from "assert";
 import { isArraySorted } from "./isarraysorted.js";
 
-describe("isArraySorted", function () {
-  describe("xxxxxxx", function () {
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([1, 2, 3, 4]);
-        assert.equal(result, true);
-      });
+describe("Testing isArraySorted function", function () {
+  describe("Positive tests", function () {
+    it("Test case 1", function () {
+      const result = isArraySorted([1, 2, 3, 4]);
+      assert.equal(result, true);
+    });
+    it("Test case 2", function () {
+      const result = isArraySorted([1, 2]);
+      assert.equal(result, true);
     });
 
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([1, 2]);
-        assert.equal(result, true);
-      });
+    it("Test case 3", function () {
+      const result = isArraySorted([1]);
+      assert.equal(result, true);
     });
 
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([1]);
-        assert.equal(result, true);
-      });
+    it("Test case 4", function () {
+      const result = isArraySorted([]);
+      assert.equal(result, true);
+    });
+  });
+
+  describe("Negative tests", function () {
+    it("Test case 5", function () {
+      const result = isArraySorted([8, 7]);
+      assert.equal(result, false);
     });
 
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([]);
-        assert.equal(result, true);
-      });
+    it("Test case 6", function () {
+      const result = isArraySorted([1, 3, 2]);
+      assert.equal(result, false);
     });
 
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([8, 7]);
-        assert.equal(result, false);
-      });
-    });
-
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([1, 3, 2]);
-        assert.equal(result, false);
-      });
-    });
-
-    describe("yyyyyyyy", function () {
-      it("zzzzzzz", function () {
-        const result = isArraySorted([1, 2, 3, 4, 100, 5, 6, 7]);
-        assert.equal(result, false);
-      });
+    it("Test case 7", function () {
+      const result = isArraySorted([1, 2, 3, 4, 100, 5, 6, 7]);
+      assert.equal(result, false);
     });
   });
 });
