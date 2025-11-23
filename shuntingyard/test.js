5c9dcba9f0f72440aaf7481a501d987d8a1cdccd
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-23 16:21:49 +0100

    progression

--- shuntingyard/test.js
+++ shuntingyard/test.js
@@ -1,31 +1,34 @@
 import assert from "assert";
 import shuntingYard from "./shuntingyard.js";
 
-describe("Shunting yard algorithm", function () {
-  describe("testing the algorithm", function () {
-    it("Test case 0", function () {
-      const result = shuntingYard("2 + 3 * 4");
-      assert.equal(result, "2 3 4 * +");
-    });
-    it("Test case 1", function () {
+describe("Testing Shunting Yard algorithm", function () {
+  describe("Tests from 'Regneøvelser med RPN'", function () {
+    it("Øvelse 1", function () {
       const result = shuntingYard("85 - 31");
       assert.equal(result, "85 31 -");
     });
-    it("Test case 2", function () {
+    it("Øvelse 2", function () {
       const result = shuntingYard("3 + 4 * ( 5 - 2 )");
       assert.equal(result, "3 4 5 2 - * +");
     });
-    it("Test case 4", function () {
+    it("Øvelse 4", function () {
       const result = shuntingYard("( 5 + 9 ) / 4");
       assert.equal(result, "5 9 + 4 /");
     });
-    it("Test case 5", function () {
+    it("Øvelse 5", function () {
       const result = shuntingYard("( 12 + 3 ) * ( 7 + 9 )");
       assert.equal(result, "12 3 + 7 9 + *");
     });
-    it("Test case 6", function () {
+    it("Øvelse 6", function () {
       const result = shuntingYard("23 ^ 2 - ( 13 * 9 ) + 5 / 7");
       assert.equal(result, "23 2 ^ 13 9 * - 5 7 / +");
+    });
+  });
+
+  describe("Other tests", function () {
+    it("Test A", function () {
+      const result = shuntingYard("2 + 3 * 4");
+      assert.equal(result, "2 3 4 * +");
     });
   });
 });
